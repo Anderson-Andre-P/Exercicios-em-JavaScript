b429029df9f250cb4a93feff17f9f755ee9d050f
Anderson André <61363255+Anderson-Andre-P@users.noreply.github.com>
2020-12-07 12:02:17 -0300

    Delete numero1.js

--- numero1.js
+++
@@ -1,4 +0,0 @@
-// Cria um input para o usuário digitar um número
-const numero = Number(prompt('Digite um número'));
-// Imprime na tela do navegador o dobro do número digitado
-document.write(`O dobro do número digitado é: ${numero * 2}`);
